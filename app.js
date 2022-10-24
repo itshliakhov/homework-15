67fb5950c0db3918dc3863ab4dfc951cedfdcec9
itshliakhov <shliakhovdan@gmail.com>
2022-10-24 22:30:40 +0300

    Adding new project

---
+++ app.js
@@ -0,0 +1,29 @@
+function Clock(clockPlace) {
+  let timer;
+  let clockContent = document.querySelector(clockPlace);
+  this.showTime = function () {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    clockContent.innerHTML = `${hours}:${minutes}:${seconds}`;
+  };
+  this.timer = setInterval(this.showTime, 1000);
+}
+Clock.prototype.stopClock = function (stopClockBtn) {
+  this.stopClockBtn = document.querySelector(stopClockBtn);
+  this.stopClockBtn.addEventListener("click", () => {
+    clearInterval(this.timer);
+  });
+};
+let clock = new Clock(".js--clock");
+clock.stopClock(".js--stopBtn");
---
+++ app2.js
@@ -0,0 +1,34 @@
+class Clock {
+  timer = undefined;
+  constructor({ clockContent, clockStopBtn }) {
+    this.clockPlace = document.querySelector(clockContent);
+    this.clockStopBtn = document.querySelector(clockStopBtn);
+  }
+  showTime = () => {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    this.clockPlace.innerHTML = `${hours}:${minutes}:${seconds}`;
+  };
+  timer = setInterval(this.showTime, 1000);
+  stopClock() {
+    this.clockStopBtn.addEventListener("click", () => {
+      clearInterval(this.timer);
+    });
+  }
+}
+let clock = new Clock({
+  clockContent: ".js--clock",
+  clockStopBtn: ".js--stopBtn",
+});
+clock.stopClock();
